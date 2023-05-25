7761905e286b0ffae0c36c60de4c7d23481239f4
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-05-24 21:28:57 -0700

    added some routes and controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,112 @@
+const { Thought, User, Reaction } = require("../models");
+
+module.exports = {
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $addToSet: { thoughts: thought._id } },
+          { new: true }
+        );
+      })
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: "No uer with that ID",
+            })
+          : res.json("Thought created!")
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this id!" })
+          : res.json(thought)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this id!" })
+          : User.findOneAndUpdate(
+              { thoughts: req.params.thoughtId },
+              { $pull: { thoughts: req.params.thoughtId } },
+              { new: true }
+            )
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: "Thought created but no user with this id!",
+            })
+          : res.json({ message: "Thought successfully deleted!" })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  addReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reaction: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this id!" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteReaction(req, res) {
+    Reaction.findOneAndRemove({ _id: req.params.reactionId })
+      .then((reaction) =>
+        !reaction
+          ? res.status(404).json({ message: "No reaction with this id!" })
+          : Thought.findOneAndUpdate(
+              { reactions: req.params.reactionId },
+              { $pull: { reactions: req.params.reactionId } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: "Reaction created but no thought with this id!",
+            })
+          : res.json({ message: "Reaction successfully deleted!" })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
--- models/User.js
+++ models/User.js
@@ -1,5 +1,37 @@
 const { Schema, Types } = require("mongoose");
+const thoughtSchema = require("./Thought");
 
-const userSchema = new Schema();
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  toJson: {
+    virtuals: true,
+  },
+});
+userSchema.virtual("friendCount").get(function () {
+  return `${this.friends.length}`;
+});
 
 module.exports = userSchema;
